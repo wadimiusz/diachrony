9b4ad8676863e4ed9a299531b9c66c4a84c27f10
wadimiusz <wadimiusz@gmail.com>
2018-12-07 17:32:51 +0300

    updates

--- naive_comparison.py
+++ naive_comparison.py
@@ -4,7 +4,28 @@ import os
 import random
 
 
-def tag_cutter(word:str):
+def load_model(embeddings_file):
+    """
+    This function, written by github.com/akutuzov, unifies various standards of word embedding files.
+    It automatically determines the format by the file extension and loads it from the disk correspondingly.
+    :param embeddings_file: path to the file
+    :return: the loaded model
+    """
+    # Determine the model format by the file extension
+    if embeddings_file.endswith('.bin.gz') or embeddings_file.endswith('.bin'):  # Binary word2vec file
+        emb_model = gensim.models.KeyedVectors.load_word2vec_format(
+            embeddings_file, binary=True, unicode_errors='replace')
+    elif embeddings_file.endswith('.txt.gz') or embeddings_file.endswith('.txt') \
+            or embeddings_file.endswith('.vec.gz') or embeddings_file.endswith('.vec'):  # Text word2vec file
+        emb_model = gensim.models.KeyedVectors.load_word2vec_format(
+            embeddings_file, binary=False, unicode_errors='replace')
+    else:  # Native Gensim format?
+        emb_model = gensim.models.KeyedVectors.load(embeddings_file)
+    emb_model.init_sims(replace=True)
+    return emb_model
+
+
+def tag_cutter(word: str):
     """
     :param word: a string with our without a tag, e g дом_NOUN or дом
     :return: the word without tags, e. g. дом
@@ -14,6 +35,33 @@ def tag_cutter(word:str):
         return word[:word.find("_")]
     else:
         return word
+
+
+def loader(w2v1_path: str, w2v2_path: str, verbose: bool):
+    """
+    This function downloads two models using load_model and, optionally, reports success.
+    :param w2v1_path: path to the first model
+    :param w2v2_path: path to the second model
+    :param verbose: defines verbosity
+    :return:
+    """
+    if not os.path.exists(w2v1_path):
+        raise FileNotFoundError("File {path} is not found".format(path=w2v1_path))
+
+    if not os.path.exists(w2v2_path):
+        raise FileNotFoundError("File {path} is not found".format(path=w2v2_path))
+
+    if verbose:
+        print("Loading the first model")
+    w2v1 = load_model(w2v1_path)
+    if verbose:
+        print("Success.")
+        print("Loading the second model")
+    w2v2 = load_model(w2v2_path)
+    if verbose:
+        print("Success.")
+
+    return w2v1, w2v2
 
 
 def get_top_neighbors(word: str, w2v: gensim.models.KeyedVectors, n: int, cut_tags: bool):
@@ -28,44 +76,29 @@ def get_top_neighbors(word: str, w2v: gensim.models.KeyedVectors, n: int, cut_ta
             result = list()
             for word in w2v.vocab:
                 if word.startswith(word + "_"):
-                    result.extend([word for word, score in w2v.most_similar(word, topn = n)])
+                    result.extend([word for word, score in w2v.most_similar(word, topn=n)])
             return result
         else:
-            return [word for word, score in w2v.most_similar(word, topn = n)]
+            return [word for word, score in w2v.most_similar(word, topn=n)]
 
 
-def comparison(w2v1_path: str, w2v2_path: str, binary1: bool, binary2: bool, top_n_neighbors: int,
+def comparison(w2v1_path: str, w2v2_path: str, top_n_neighbors: int,
                top_n_changed_words: (int, None), top_n_most_frequent_words: (int, None), pos_tag: str,
                verbose: bool, cut_tags: bool):
     """
     This module extracts two models from two specified paths and compares the meanings of words within their vocabulary.
     :param w2v1_path: the path to the first model
     :param w2v2_path: the path to the second model
-    :param binary1: if True, open the first model as binary
-    :param binary2: if True, open the second model as binary
     :param top_n_neighbors: we will compare top n neighbors of words
     :param top_n_changed_words: we will output top n most interesting words, may be int or None
     :param top_n_most_frequent_words: we will use top n most frequent words from each model, may be int or None
     :param pos_tag: specify this to consider only words with a specific pos_tag
     :param verbose: if True the model is verbose (gives system messages)
+    :param cut_tags: if True words like дом_NOUN turn into words like дом, without tags
     :return: None
     """
 
-    if not os.path.exists(w2v1_path):
-        raise FileNotFoundError("File {path} is not found".format(path=w2v1_path))
-
-    if not os.path.exists(w2v2_path):
-        raise FileNotFoundError("File {path} is not found".format(path=w2v2_path))
-
-    if verbose:
-        print("Loading the first model")
-    w2v1 = gensim.models.KeyedVectors.load_word2vec_format(w2v1_path, binary=binary1)
-    if verbose:
-        print("Success.")
-        print("Loading the second model")
-    w2v2 = gensim.models.KeyedVectors.load_word2vec_format(w2v2_path, binary=binary2)
-    if verbose:
-        print("Success.")
+    w2v1, w2v2 = loader(w2v1_path=w2v1_path, w2v2_path=w2v2_path, verbose=verbose)
 
     vocab1 = list(w2v1.vocab.keys())
     vocab2 = list(w2v2.vocab.keys())
@@ -87,7 +120,7 @@ def comparison(w2v1_path: str, w2v2_path: str, binary1: bool, binary2: bool, top
     vocab1 = vocab1[:top_n_most_frequent_words]
     vocab2 = vocab2[:top_n_most_frequent_words]
 
-    shared_vocabulary = [word for word in vocab1 if word in vocab2]
+    shared_vocabulary = list(set(vocab1).intersection(set(vocab2)))
     if verbose:
         print("The shared vocabulary contains {n} words".format(n=len(shared_vocabulary)))
 
@@ -118,12 +151,8 @@ def comparison(w2v1_path: str, w2v2_path: str, binary1: bool, binary2: bool, top
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model1', type=str, help="the path to the first model")
-    parser.add_argument('--binary1', action="store_true", help="If this argument if given, the first model"
-                                                               " is read is binary")
-    parser.add_argument('--model2', type=str, help="the path to the first model")
-    parser.add_argument('--binary2', action="store_true", help="If this argument if given, the second model"
-                                                               " is read is binary")
+    parser.add_argument('--model1', type=str, help="the path to the first model", required=True)
+    parser.add_argument('--model2', type=str, help="the path to the first model", required=True)
     parser.add_argument("--top-n-neighbors", type=int, default=10, help="We will compare top n nearest neighbors",
                         dest="top_n_neighbors")
 
@@ -143,10 +172,9 @@ def main():
                                                                 "дом_NOUN --> дом")
 
     args = parser.parse_args()
-    comparison(w2v1_path=args.model1, w2v2_path=args.model2, binary1=args.binary1, binary2=args.binary2,
-               top_n_neighbors=args.top_n_neighbors, top_n_most_frequent_words=args.top_n_most_frequent_words,
-               pos_tag=args.pos_tag, verbose=args.verbose, top_n_changed_words=args.top_n_changed_words,
-               cut_tags=args.cut_tags)
+    comparison(w2v1_path=args.model1, w2v2_path=args.model2, top_n_neighbors=args.top_n_neighbors,
+               top_n_most_frequent_words=args.top_n_most_frequent_words, pos_tag=args.pos_tag, verbose=args.verbose,
+               top_n_changed_words=args.top_n_changed_words, cut_tags=args.cut_tags)
 
 
 if __name__ == "__main__":
--- simple_comparison.sh
+++ simple_comparison.sh
@@ -1,3 +1,3 @@
 alias python=python3.6
 python --version
-python naive_comparison.py --model1 models/news_win20.model.bin --binary1 --model2 models/news_0_300_2.bin --binary2 --top-n-changed-words 10 --verbose --cut-tags
+python naive_comparison.py --model1 models/2012.model --model2 models/2013.model --top-n-changed-words 10 --verbose
