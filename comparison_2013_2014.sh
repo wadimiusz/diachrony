6d0ec3448275cf021174c7a0fd825852b1e5ee07
wadimiusz <wadimiusz@gmail.com>
2019-01-17 03:01:44 +0300

    typo

--- comparison_2013_2014.sh
+++ comparison_2013_2014.sh
@@ -1,1 +1,1 @@
-python3 naive_comparison.py --model1 wordvectors/2013.model --model2 wordvectors/2014.model --top-n-changed-words 10 --verbose --pos-tag ADJ --top-n-most-frequent-words 1000 --top-n-neighbors 50
+python3 main.py --model1 wordvectors/2013.model --model2 wordvectors/2014.model --top-n-changed-words 10 --verbose --pos-tag ADJ --top-n-most-frequent-words 1000 --top-n-neighbors 50
