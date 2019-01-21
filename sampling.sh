4c0d43200c52fbfc86088e81e0038faec4b1da8b
wadimiusz <wadimiusz@gmail.com>
2019-01-21 22:38:17 +0300

    samplig.sh modified

--- sampling.sh
+++ sampling.sh
@@ -1,2 +1,2 @@
-python3 sampling.py --models wordvectors/*.model --pos-tag NOUN --top-n-most-frequent-words 1000 --shuffle --positive-samples 10 --negative-samples 10 | tee samples.txt
-cat samples.txt | nice -n 20 python3 creating_corpus.py | tee corpus.txt
+python3 sampling.py --models wordvectors/*.model --pos-tag NOUN --top-n-most-frequent-words 1000 --shuffle --positive-samples 10 --negative-samples 10 | tee samples.txt 2> /dev/null
+cat samples.txt | nice -n 20 python3 creating_corpus.py | tee corpus.txt 2> /dev/null
