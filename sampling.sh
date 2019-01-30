e63f2aaa9ca4fe11c5eb57ea2ee9e131d8dee986
wadimiusz <wadimiusz@gmail.com>
2019-01-30 14:21:21 +0300

    dataset modified

--- sampling.sh
+++ sampling.sh
@@ -1,2 +1,2 @@
-python3 sampling.py --models wordvectors/*.model --pos-tag NOUN --top-n-most-frequent-words 1000 --shuffle --positive-samples 10 --negative-samples 10 | tee samples.txt 2> /dev/null
-cat samples.txt | nice -n 20 python3 creating_corpus.py | tee corpus.txt 2> /dev/null
+python3 sampling.py --models wordvectors/*.model --pos-tag ADJ --top-n-most-frequent-words 1000 --shuffle --positive-samples 10 --negative-samples 10 | tee dataset/samples.txt 2> /dev/null
+cat dataset/samples.txt | nice -n 20 python3 creating_corpus.py | tee dataset/corpus.txt 2> /dev/null
