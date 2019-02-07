a890d99c40f10c09e41acd32f13e4974c8036790
juliarodina <44140496+juliarodina@users.noreply.github.com>
2019-02-07 17:26:28 +0300

    Update annotation.py

--- utils/annotation.py
+++ utils/annotation.py
@@ -3,27 +3,19 @@ import os
 import pandas as pd
 import sys
 
-with open(sys.argv[1], 'r') as infile:
-    reader = csv.DictReader(infile)
-    data = {}
-    for row in reader:
-        for header, value in row.items():
-            try:
-                data[header].append(value)
-            except KeyError:
-                data[header] = [value]
+corpus = pd.read_csv(sys.argv[1], sep = ',', index_col = ['ID'])
+samples = pd.read_csv(sys.argv[2], sep = ',', index_col = ['ID'])
 
-samples = pd.read_csv(sys.argv[2])
 
-words = list(zip(data['ID'], data['WORD'], data['OLD_CONTEXTS'], data['NEW_CONTEXTS']))
+for index in corpus.index:
+    print(corpus.loc[index, 'WORD'], '\n', corpus.loc[index, 'OLD_CONTEXTS'], '\n', corpus.loc[index, 'NEW_CONTEXTS'], '\n')
 
-for item in words:
-    print(item[1], '\n', item[2], '\n', item[3], '\n')
     answer = input(
-        "Оцените, насколько изменилось значение/употребление слова от 0 (совсем не изменилось) до 3 (полностью изменилось): ")
+      "Оцените, насколько изменилось значение/употребление слова от 0 (совсем не изменилось) до 2 (полностью изменилось): ")
     if answer == 'стоп':
         break
-    result = samples.set_value(int(item[0]), 'ASSESSOR_LABEL', answer)
+
+    result = samples.set_value(index, 'ASSESSOR_LABEL', answer)
     samples.update(result)
 
 samples.to_csv('results.csv')
