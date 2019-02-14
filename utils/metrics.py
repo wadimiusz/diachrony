863c3cb240befe27298a581db23c873b73345ebf
wadimiusz <wadimiusz@gmail.com>
2019-02-15 00:44:08 +0300

    metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,49 +1,56 @@
-import sys
-import numpy as np
+import os, sys
 import pandas as pd
 from sklearn.metrics import cohen_kappa_score, mean_squared_error
+from nltk import agreement
 
+words = pd.read_csv('dataset/annotated/samples_ADJ_daria.csv')['WORD']
+daria = pd.read_csv('dataset/annotated/samples_ADJ_daria.csv')['ASSESSOR_LABEL']
+julia = pd.read_csv('dataset/annotated/samples_ADJ_julia.csv')['ASSESSOR_LABEL']
+vadim = pd.read_csv('dataset/annotated/samples_ADJ_vadim.csv')['ASSESSOR_LABEL']
+correct = pd.read_csv('dataset/annotated/samples_ADJ_vadim.csv')['LABEL']
+mean = pd.concat([julia, daria, vadim], axis=1).mean(axis=1)
+median = pd.concat([julia, daria, vadim], axis=1).median(axis=1)
 
-def read_files(data):
-    samples = pd.read_csv(data)
-    binary_samples = pd.read_csv(data)
+print("Daria labels stats:", daria.value_counts(), sep='\n', end='\n\n')
+print("Julia labels stats:", julia.value_counts(), sep='\n', end='\n\n')
+print("Vadim labels stats:", vadim.value_counts(), sep='\n', end='\n\n')
+print("========================================")
+print('Agreement between Vadim and Julia (3 labels):', cohen_kappa_score(vadim, julia))
+print('Agreement between Vadim and Daria (3 labels):', cohen_kappa_score(vadim, daria))
+print('Agreement between Julia and Daria (3 labels):', cohen_kappa_score(julia, daria), end='\n\n')
 
-    for index in binary_samples.index:
-        if binary_samples.loc[index, 'ASSESSOR_LABEL'] == 2:
-            result = binary_samples.set_value(index, 'ASSESSOR_LABEL', 1)
-            binary_samples.update(result)
-        else:
-            continue
+print('Agreement between Vadim and Julia (2 labels):', cohen_kappa_score(vadim > 0, julia > 0))
 
-    return samples, binary_samples
+print('Agreement between Vadim and Daria (2 labels):', cohen_kappa_score(vadim > 0, daria > 0))
 
+print('Agreement between Julia and Daria (2 labels):', cohen_kappa_score(julia > 0, daria > 0), end='\n\n')
 
-def cohen_kappa(data1, data2):
+print("The cohen cappa of random guessing is 0.")
+print("========================================")
+print("Mean squared error:")
+print("Label / 2:")
+print("Julia:", mean_squared_error(correct, julia / 2))
+print("Daria:", mean_squared_error(correct, daria / 2))
+print("Vadim:", mean_squared_error(correct, vadim / 2))
+print("Mean:", mean_squared_error(correct, mean / 2))
+print("Median:", mean_squared_error(correct, median / 2), end='\n\n')
 
-    k = cohen_kappa_score(data1['ASSESSOR_LABEL'], data2['ASSESSOR_LABEL'])
+print("Label > 0:")
+print("Julia:", mean_squared_error(correct, julia > 0))
+print("Daria:", mean_squared_error(correct, daria > 0))
+print("Vadim:", mean_squared_error(correct, vadim > 0))
+print("Mean:", mean_squared_error(correct, mean > 0))
+print("Median:", mean_squared_error(correct, median > 0), end='\n\n')
 
-    return k
+print("Label > 1:")
+print("Julia:", mean_squared_error(correct, julia > 1))
+print("Daria:", mean_squared_error(correct, daria > 1))
+print("Vadim:", mean_squared_error(correct, vadim > 1))
+print("Mean:", mean_squared_error(correct, mean > 1))
+print("Median:", mean_squared_error(correct, median > 1), end='\n\n')
 
-
-def mean_sqr_error(data):
-
-    mse = mean_squared_error(data['LABEL'],
-                             data['ASSESSOR_LABEL'])
-
-    return mse
-
-
-for i in range(1, 3):  # 3 поменять на 4
-    results, results_binary = read_files(sys.argv[i])
-    print('Mean square error annotator(binary)/algorithm: ', mean_sqr_error(results_binary))
-
-# Оптимизируйте плиз как-то это говно
-i = 1
-results, results_binary = read_files(sys.argv[i])
-results2, results_binary2 = read_files(sys.argv[i+1])
-#results3, results_binary3 = read_files(sys.argv[i+2])
-print('Cohen\'s kappa scores: ', cohen_kappa(results, results2))
-#print('Cohen\'s kappa scores: ', cohen_kappa(results, results2), '\n', cohen_kappa(results, results3),
-#      '\n', cohen_kappa(results2, results3))
-#print('Mean kappa: ', np.mean())
-
+print("The MSE of random guessing here is 0.5.")
+print("========================================")
+print("Frequent words")
+counts = words[correct == 1].value_counts()
+print(counts[counts > 1])
