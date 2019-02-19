d039cf595648a357024b1b8d3941d730a871a090
wadimiusz <wadimiusz@gmail.com>
2019-02-19 21:52:34 +0300

    eval

--- eval.py
+++ eval.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.metrics import f1_score
 
 from models import GlobalAnchors
 from models import ProcrustesAligner
@@ -35,9 +36,19 @@ df = pd.read_csv('dataset/annotated.csv')
 df_longterm = pd.read_csv('dataset/gold_kutuzov_kuzmenko_2017.tsv')
 
 scores = dict()
+f1_macro = pd.DataFrame({"model": ["GlobalAnchors", "ProcrustesAligner", "KendallTau", "Jaccard", "united"]})
+f1_macro.index.name="id"
+
+f1_for_2 = pd.DataFrame({"model": ["GlobalAnchors", "ProcrustesAligner", "KendallTau", "Jaccard", "united"]})
+f1_for_2.index.name="id"
 for kind in ['regular', 'incremental']:
-    for Scorer in [GlobalAnchors, ProcrustesAligner, KendallTau, Jaccard]:
-        scores[(kind, Scorer)] = list()
+    max_scorers = 4
+    max_samples = len(df)
+    X = np.ndarray((max_samples, max_scorers))
+    y = np.array(df["GROUND_TRUTH"])
+    current_f1_macro = list()
+    current_f1_for_2 = list()
+    for scorer_num, Scorer in enumerate([GlobalAnchors, ProcrustesAligner, KendallTau, Jaccard]):
         for idx, values in df.iterrows():
             print("{kind}, {scorer}, {idx} / {max}".format(kind=kind, scorer=str(Scorer), idx=idx, max=280),
                   file=sys.stderr)
@@ -47,10 +58,29 @@ for kind in ['regular', 'incremental']:
 
             model1, model2 = get_models_by_year(year, kind)
             score = Scorer(w2v1=model1, w2v2=model2, top_n_neighbors=50).get_score(word)
-            scores[(kind, Scorer)].append(score)
+            X[idx, scorer_num] = score
 
-        X = np.array(scores[(kind, Scorer)]).reshape((-1, 1))
-        clf = DecisionTreeClassifier(max_depth=5).fit(X, y=df["GROUND_TRUTH"])
-        print("Word vectors are {kind}, scores are from {scorer}".format(kind=kind, scorer=str(Scorer)))
-        print("Sanity check")
-        print(classification_report(df["GROUND_TRUTH"], clf.predict(X)))
+        clf = DecisionTreeClassifier(max_depth=5)
+        fold = StratifiedKFold(9, shuffle=False)
+        model_f1 = np.mean(cross_val_score(clf, X[:, scorer_num].reshape(-1, 1), y, scoring='f1_macro', cv=fold))
+        current_f1_macro.append(model_f1)
+        model_f1_for_2 = np.mean(cross_val_score(
+            clf, X[:, scorer_num].reshape(-1, 1), y, scoring=lambda estimator, X, y: f1_score(
+                estimator.predict(X), y, average='macro', pos_label=1, labels=[1]), cv=fold))
+
+        current_f1_for_2.append(model_f1_for_2)
+
+    clf = DecisionTreeClassifier(max_depth=5)
+    fold = StratifiedKFold(9, shuffle=False)
+    united_f1 = np.mean(cross_val_score(clf, X, y, scoring='f1_macro', cv=fold))
+    current_f1_macro.append(united_f1)
+    f1_macro[kind] = current_f1_macro
+    united_f1_for_2 = np.mean(cross_val_score(
+            clf, X, y, scoring=lambda estimator, X, y: f1_score(
+                estimator.predict(X), y, average='macro', pos_label=1, labels=[1]), cv=fold))
+
+    current_f1_for_2.append(united_f1_for_2)
+    f1_for_2[kind] = current_f1_for_2
+
+f1_macro.to_csv('outputs/f1_macro.csv')
+f1_for_2.to_csv('outputs/f1_for_2.csv')
