631288a50bbd4d17c2ca726087e7934594144ccc
juliarodina <44140496+juliarodina@users.noreply.github.com>
2019-04-10 00:45:47 +0300

    very global such anchors

--- comparing_adjectives/plot_results.py
+++ comparing_adjectives/plot_results.py
@@ -2,12 +2,14 @@ import numpy as np
 from matplotlib import pyplot
 import pandas as pd
 
-evaluative = pd.read_csv("cos_dist_eval.csv")
+#evaluative = pd.read_csv("cos_dist_eval.csv")
 #rest = pd.read_csv("cos_dist_rest.csv")
-rest = pd.read_csv("cos_dist_rest_notfiltered.csv")
+evaluative = pd.read_csv("globalanchors_eval.csv")
+rest = pd.read_csv("globalanchors_rest.csv")
 
-x = [i for i in evaluative['mean_dist'].tolist()]  # if i != 0.0]
-y = [i for i in rest['mean_dist'].tolist()]  # if i != 0.0]
+
+x = [i for i in evaluative['mean_ga'].tolist()]  # if i != 0.0]
+y = [i for i in rest['mean_ga'].tolist()]  # if i != 0.0]
 
 bins = np.linspace(0, 1, 50)
 
@@ -16,4 +18,4 @@ pyplot.hist(y, bins, alpha=0.5, label='rest', ec='black')
 pyplot.legend(loc='upper right')
 #pyplot.show()
 #pyplot.savefig('speedchange_cosdist.png')
-pyplot.savefig('speedchange_cosdist_notfilt.png')
+pyplot.savefig('speedchange_globalanchors.png')
