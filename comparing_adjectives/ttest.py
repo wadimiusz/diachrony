9b3e6c7950b14f38ece22df29e9cbf4ae1a7291d
Andrey Kutuzov <akutuzov72@gmail.com>
2019-04-18 18:18:23 +0200

    T-test simplification

--- comparing_adjectives/ttest.py
+++ comparing_adjectives/ttest.py
@@ -14,6 +14,15 @@ distances1 = []
 distances0_2 = []
 distances1_2 = []
 
+deviations0 = []
+deviations1 = []
+
+sums0 = []
+sums1 = []
+
+sums0_2 = []
+sums1_2 = []
+
 
 for line in open(file0, 'r').readlines()[1:]:
     res = line.strip().split(',')
@@ -23,6 +32,15 @@ for line in open(file0, 'r').readlines()[1:]:
     distance0_2 = float(res[4])
     if distance0_2:
         distances0_2.append(distance0_2)
+    deviation0 = float(res[5])
+    if deviation0:
+        deviations0.append(deviation0)
+    sum0 = float(res[6])
+    if sum0:
+        sums0.append(sum0)
+    sum0_2 = float(res[7])
+    if sum0_2:
+        sums0_2.append(sum0_2)
 
 for line in open(file1, 'r').readlines()[1:]:
     res = line.strip().split(',')
@@ -32,78 +50,30 @@ for line in open(file1, 'r').readlines()[1:]:
     distance1_2 = float(res[4])
     if distance1_2:
         distances1_2.append(distance1_2)
-
-deviations0 = []
-deviations1 = []
-
-for line in open(file0, 'r').readlines()[1:]:
-    res = line.strip().split(',')
-    distance = float(res[5])
-    if distance:
-        deviations0.append(distance)
-
-for line in open(file1, 'r').readlines()[1:]:
-    res = line.strip().split(',')
-    distance = float(res[5])
-    if distance:
-        deviations1.append(distance)
-
-sums0 = []
-sums1 = []
-
-for line in open(file0, 'r').readlines()[1:]:
-    res = line.strip().split(',')
-    distance = float(res[6])
-    if distance:
-        sums0.append(distance)
-
-for line in open(file1, 'r').readlines()[1:]:
-    res = line.strip().split(',')
-    distance = float(res[6])
-    if distance:
-        sums1.append(distance)
-
-sums0_2 = []
-sums1_2 = []
-
-for line in open(file0, 'r').readlines()[1:]:
-    res = line.strip().split(',')
-    distance = float(res[7])
-    if distance:
-        sums0_2.append(distance)
-
-for line in open(file1, 'r').readlines()[1:]:
-    res = line.strip().split(',')
-    distance = float(res[7])
-    if distance:
-        sums1_2.append(distance)
+    deviation1 = float(res[5])
+    if deviation1:
+        deviations1.append(deviation1)
+    sum1 = float(res[6])
+    if sum1:
+        sums1.append(sum1)
+    sum1_2 = float(res[7])
+    if sum1_2:
+        sums1_2.append(sum1_2)
 
 print('Comparing', file0, file1)
 
-print('=====')
-print('Distances_procrustes')
-print('Averages: %.3f \t %.3f' % (np.average(distances0), np.average(distances1)))
-print('T-test: %.5f \t %.5f' % test(distances0, distances1))
+comparisons = \
+    [(distances0, distances1), (distances0_2, distances1_2),
+     (deviations0, deviations1), (sums0, sums1), (sums0_2, sums1_2)]
+comparison_names = \
+    ['Procrustes distances', 'Global Anchors distances', 'Standard deviations',
+     'Sum of Procrustes deltas', 'Sum of Global Anchors deltas']
 
-print('=====')
-print('Distances_globalanchors')
-print('Averages: %.3f \t %.3f' % (np.average(distances0_2), np.average(distances1_2)))
-print('T-test: %.5f \t %.5f' % test(distances0_2, distances1_2))
-
-print('=====')
-print('Standard deviations')
-print('Averages: %.3f \t %.3f' % (np.average(deviations0), np.average(deviations1)))
-print('T-test: %.5f \t %.5f' % test(deviations0, deviations1))
-
-print('=====')
-print('Sum of deltas procrustes')
-print('Averages: {0:.3f} \t {0:.3f}'.format(np.average(sums0), np.average(sums1)))
-print('T-test: %.5f \t %.5f' % test(sums0, sums1))
-
-print('=====')
-print('Sum of deltas globalanchors')
-print('Averages: %.3f \t %.3f' % (np.average(sums0_2), np.average(sums1_2)))
-print('T-test: %.5f \t %.5f' % test(sums0_2, sums1_2))
+for pair, name in zip(comparisons, comparison_names):
+    print('=====')
+    print(name)
+    print('Averages: %.3f \t %.3f' % (np.average(pair[0]), np.average(pair[1])))
+    print('T-test (difference and p-value): %.5f, %.5f' % test(pair[0], pair[1]))
 
 
 
