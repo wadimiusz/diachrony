c3f41f2d7afc49c4407b849f5b797b108f4de3e3
Andrey Kutuzov <akutuzov72@gmail.com>
2019-04-18 18:45:30 +0200

    Formatting in get_adj

--- comparing_adjectives/get_adjs.py
+++ comparing_adjectives/get_adjs.py
@@ -8,8 +8,9 @@ from scipy.stats import percentileofscore
 
 def load_model(embeddings_file):
     """
-    This function, written by github.com/akutuzov, unifies various standards of word embedding files.
-    It automatically determines the format by the file extension and loads it from the disk correspondingly.
+    This function, unifies various standards of word embedding files.
+    It automatically determines the format by the file extension
+    and loads it from the disk correspondingly.
     :param embeddings_file: path to the file
     :return: the loaded model
     """
@@ -18,11 +19,13 @@ def load_model(embeddings_file):
     if not os.path.isfile(embeddings_file):
         raise FileNotFoundError("No file called {file}".format(file=embeddings_file))
     # Determine the model format by the file extension
-    if embeddings_file.endswith('.bin.gz') or embeddings_file.endswith('.bin'):  # Binary word2vec file
+    # Binary word2vec file:
+    if embeddings_file.endswith('.bin.gz') or embeddings_file.endswith('.bin'):
         emb_model = gensim.models.KeyedVectors.load_word2vec_format(
             embeddings_file, binary=True, unicode_errors='replace')
+    # Text word2vec file:
     elif embeddings_file.endswith('.txt.gz') or embeddings_file.endswith('.txt') \
-            or embeddings_file.endswith('.vec.gz') or embeddings_file.endswith('.vec'):  # Text word2vec file
+            or embeddings_file.endswith('.vec.gz') or embeddings_file.endswith('.vec'):
         emb_model = gensim.models.KeyedVectors.load_word2vec_format(
             embeddings_file, binary=False, unicode_errors='replace')
     else:  # Native Gensim format?
@@ -32,14 +35,14 @@ def load_model(embeddings_file):
     return emb_model
 
 
-def get_models_by_decade(decade: int, kind: str):
+def get_models_by_decade(cur_decade: int, kind: str):
     if kind not in ['regular', 'incremental']:
         raise ValueError
 
     if kind == "regular":
-        model = load_model('data/models/{decade}.model'.format(decade=decade))
+        model = load_model('data/models/{decade}.model'.format(decade=cur_decade))
     else:
-        model = load_model('data/models/{decade}_incremental.model'.format(decade=decade))
+        model = load_model('data/models/{decade}_incremental.model'.format(decade=cur_decade))
 
     return model
 
@@ -59,7 +62,7 @@ vocabs_incremental = [model.wv.vocab for model in models_incremental]
 intersec_regular = set.intersection(*map(set, vocabs_regular))
 intersec_incremental = set.intersection(*map(set, vocabs_incremental))
 
-#print(len(intersec_regular), len(intersec_incremental))
+# print(len(intersec_regular), len(intersec_incremental))
 
 
 words_regular = []
@@ -67,14 +70,15 @@ words_incremental = []
 eval_adj = pd.read_csv(sys.argv[1])
 tag = sys.argv[2]
 for line in eval_adj['WORD']:
-    word = line + '_' + tag
-    if word in intersec_regular:
-        words_regular.append(word)
-    if word in intersec_incremental:
-        words_incremental.append(word)
+    voc_word = line + '_' + tag
+    if voc_word in intersec_regular:
+        words_regular.append(voc_word)
+    if voc_word in intersec_incremental:
+        words_incremental.append(voc_word)
 
-#print(len(words_regular), len(words_incremental))
-#print(words_regular[:10], words_intersec[:10])
+
+# print(len(words_regular), len(words_incremental))
+# print(words_regular[:10], words_intersec[:10])
 
 
 def delete_lowfreqent(wordlist, treshold, vocablist):
@@ -83,26 +87,28 @@ def delete_lowfreqent(wordlist, treshold, vocablist):
         counts = []
         for vocab in vocablist:
             counts.append(vocab[word].count)
-        if sum(counts) >= treshold*len(vocablist):
+        if sum(counts) >= treshold * len(vocablist):
             newlist.append(word)
 
     return newlist
 
 
 words_regular_filtered = delete_lowfreqent(words_regular, int(sys.argv[3]), vocabs_regular)
-words_incremental_filtered = delete_lowfreqent(words_incremental, int(sys.argv[3]), vocabs_incremental)
-
-#print(len(words_regular_filtered), len(words_incremental_filtered))
-#print(words_regular_filtered[:10], words_incremental_filtered[:10])
+words_incremental_filtered = delete_lowfreqent(words_incremental, int(sys.argv[3]),
+                                               vocabs_incremental)
 
 
-def get_freqdict(wordlist, vocablist, corpus_len):
+# print(len(words_regular_filtered), len(words_incremental_filtered))
+# print(words_regular_filtered[:10], words_incremental_filtered[:10])
+
+
+def get_freqdict(wordlist, vocablist, corpus_size):
     all_freqs = []
     word_freq = {}
     for word in wordlist:
         counts = [vocab[word].count for vocab in vocablist]
-        frequency = [counts[i] / corpus_len[i] for i in range(len(vocablist))]
-        mean_frequency = sum(frequency)/len(frequency)
+        frequency = [counts[i] / corpus_size[i] for i in range(len(vocablist))]
+        mean_frequency = sum(frequency) / len(frequency)
         all_freqs.append(mean_frequency)
         word_freq[word] = mean_frequency
 
@@ -113,12 +119,13 @@ def get_freqdict(wordlist, vocablist, corpus_len):
     return percentiles
 
 
-corpus_len = [int(i) for i in sys.argv[4:8+1]]
+corpus_len = [int(i) for i in sys.argv[4:8 + 1]]
 
 wordfreq_regular = get_freqdict(words_regular, vocabs_regular, corpus_len)
 wordfreq_incremental = get_freqdict(words_incremental, vocabs_incremental, corpus_len)
 wordfreq_regular_filtered = get_freqdict(words_regular_filtered, vocabs_regular, corpus_len)
-wordfreq_incremental_filtered = get_freqdict(words_incremental_filtered, vocabs_incremental, corpus_len)
+wordfreq_incremental_filtered = \
+    get_freqdict(words_incremental_filtered, vocabs_incremental, corpus_len)
 
 '''
 for x in list(wordfreq_regular)[0:5]:
@@ -130,20 +137,22 @@ for x in list(wordfreq_incremental_filtered)[0:5]:
 
 rest_regular = []
 rest_incremental = []
-for word in intersec_regular:
-    if word.endswith(tag) and word not in words_regular:
-        rest_regular.append(word)
-for word in intersec_incremental:
-    if word.endswith(tag) and word not in words_incremental:
-        rest_incremental.append(word)
+for voc_word in intersec_regular:
+    if voc_word.endswith(tag) and voc_word not in words_regular:
+        rest_regular.append(voc_word)
+for voc_word in intersec_incremental:
+    if voc_word.endswith(tag) and voc_word not in words_incremental:
+        rest_incremental.append(voc_word)
 
 rest_regular_filtered = delete_lowfreqent(rest_regular, int(sys.argv[3]), vocabs_regular)
-rest_incremental_filtered = delete_lowfreqent(rest_incremental, int(sys.argv[3]), vocabs_incremental)
+rest_incremental_filtered = delete_lowfreqent(rest_incremental, int(sys.argv[3]),
+                                              vocabs_incremental)
 
 restfreq_regular = get_freqdict(rest_regular, vocabs_regular, corpus_len)
 restfreq_incremental = get_freqdict(rest_incremental, vocabs_incremental, corpus_len)
 restfreq_regular_filtered = get_freqdict(rest_regular_filtered, vocabs_regular, corpus_len)
-restfreq_incremental_filtered = get_freqdict(rest_incremental_filtered, vocabs_incremental, corpus_len)
+restfreq_incremental_filtered = get_freqdict(rest_incremental_filtered, vocabs_incremental,
+                                             corpus_len)
 
 '''
 for x in list(restfreq_regular)[0:5]:
@@ -159,11 +168,11 @@ def output_results(evaluative_dict, rest_dict):
     finallist = []
     temp = []
     for i in evaluative_dict:
-        l = 0
+        counter = 0
         for j in rest_dict:
-            if evaluative_dict[i] == rest_dict[j] and l < 2 and j not in temp:
+            if evaluative_dict[i] == rest_dict[j] and counter < 2 and j not in temp:
                 finallist.append(j)
-                l += 1
+                counter += 1
                 temp.append(j)
     df['WORD'] = finallist
 
