a8e8289e4eedc2b352cb7fe54e6ce189e50a44e8
Andrey Kutuzov <akutuzov72@gmail.com>
2019-04-18 22:03:59 +0200

    Fixed get_move_from_initial_GA()

--- comparing_adjectives/comparing.py
+++ comparing_adjectives/comparing.py
@@ -65,16 +65,20 @@ def get_move_from_initial_procrustes(wordlist, modellist):
 def get_move_from_initial_globalanchors(wordlist, modellist):
     move_from_init = {}
     for word in wordlist:
-        deltas = []
-        current = 0
+        distances = []
         for i in range(1, len(modellist)):
-            delta = GlobalAnchors(w2v1=modellist[0], w2v2=modellist[i]).get_score(word) - current
-            current = GlobalAnchors(w2v1=modellist[0], w2v2=modellist[i]).get_score(word)
-            if i == 1:
-                deltas.append(1 - delta)
-            else:
-                deltas.append(- delta)
-        move_from_init[word] = np.sum(deltas)
+            similarity = GlobalAnchors(w2v1=modellist[0], w2v2=modellist[i]).get_score(word)
+            distance = 1 - similarity
+            distances.append(distance)
+        deltas = 0
+        previous = distances[0]
+        for d in distances[1:]:
+            if d > previous:
+                deltas += 1
+            elif d < previous:
+                deltas -= 1
+            previous = d
+        move_from_init[word] = deltas / (len(distances) - 1)
 
     return move_from_init
 
